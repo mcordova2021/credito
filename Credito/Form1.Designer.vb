b5bdf5bb7f009d03f93b8176be6fa752c5e4d494
mcordova2021 <macs2021@hotmail.com>
2017-07-13 22:40:11 -0500

    Se añadieron los archivos fuente al repositorio
    Se subieron los 5 directorios y 28 archivos del código fuente al repositorio.

---
+++ Credito/Form1.Designer.vb
@@ -0,0 +1,381 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(184, 96)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(52, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(252, 193)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 39)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Establecer crédito"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(184, 137)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(52, 23)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(184, 180)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(52, 23)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(184, 220)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(52, 23)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(184, 263)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(52, 23)
+        Me.TextBox5.TabIndex = 5
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(184, 302)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(52, 23)
+        Me.TextBox6.TabIndex = 6
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(552, 80)
+        Me.TextBox7.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox7.TabIndex = 7
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(552, 122)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox8.TabIndex = 8
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(552, 164)
+        Me.TextBox9.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox9.TabIndex = 9
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(552, 204)
+        Me.TextBox10.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox10.TabIndex = 10
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(552, 246)
+        Me.TextBox11.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox11.TabIndex = 11
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(552, 287)
+        Me.TextBox12.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox12.TabIndex = 12
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Location = New System.Drawing.Point(552, 331)
+        Me.TextBox13.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(48, 23)
+        Me.TextBox13.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 99)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 15)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Experiencia en rubro"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 143)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(93, 15)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Persona natural"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 182)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 15)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Persona jurídica"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 224)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(139, 15)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "Trabajador dependiente"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(27, 268)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(149, 15)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Trabajador independiente"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 308)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(101, 15)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Domicilio estable"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(352, 83)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(116, 15)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Empresarial Leasing"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(352, 126)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(150, 15)
+        Me.Label8.TabIndex = 21
+        Me.Label8.Text = "Empresarial Agropecuario"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(352, 167)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(108, 15)
+        Me.Label9.TabIndex = 22
+        Me.Label9.Text = "Consumo Personal"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(352, 208)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(112, 15)
+        Me.Label10.TabIndex = 23
+        Me.Label10.Text = "Consumo Convenio"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(352, 249)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(85, 15)
+        Me.Label11.TabIndex = 24
+        Me.Label11.Text = "Consumo Casa"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(352, 293)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(170, 15)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "Confines vivienia - Mi Vivienda"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(352, 335)
+        Me.Label13.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(179, 15)
+        Me.Label13.TabIndex = 26
+        Me.Label13.Text = "Confines vivienia - Techo propio"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(146, 30)
+        Me.Label14.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(293, 15)
+        Me.Label14.TabIndex = 27
+        Me.Label14.Text = "APLICACIÓN PARA ESTABLECER TIPO DE CRÉDITOS"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(632, 390)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox13)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Candara", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Tipo de Crédito"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox10 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox11 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox12 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox13 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+
+End Class
---
+++ Credito/Form1.vb
@@ -0,0 +1,257 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim netsum As Double
+        Dim inarray(6) As Integer
+        Dim outarray(7) As Integer
+        Static feature2(9) As Double
+
+        ' inarray(1) is EA
+        ' inarray(2) is PN
+        ' inarray(3) is PJ
+        ' inarray(4) is D
+        ' inarray(5) is I
+        ' inarray(6) is DE
+        ' outarray(1) is EL
+        ' outarray(2) is EA
+        ' outarray(3) is CP
+        ' outarray(4) is CC
+        ' outarray(5) is CC
+        ' outarray(6) is CVV
+        ' outarray(7) is CVTP
+
+        inarray(1) = Val(Me.TextBox1.Text)
+        inarray(2) = Val(Me.TextBox2.Text)
+        inarray(3) = Val(Me.TextBox3.Text)
+        inarray(4) = Val(Me.TextBox4.Text)
+        inarray(5) = Val(Me.TextBox5.Text)
+        inarray(6) = Val(Me.TextBox6.Text)
+
+        If (inarray(1) < 0) Then inarray(1) = 0
+        If (inarray(1) > 1) Then inarray(1) = 1
+        inarray(1) = inarray(1)
+
+        If (inarray(2) < 1) Then inarray(2) = 1
+        If (inarray(2) > 2) Then inarray(2) = 2
+        inarray(2) = (inarray(2) - 1)
+
+        If (inarray(3) < 0) Then inarray(3) = 0
+        If (inarray(3) > 1) Then inarray(3) = 1
+        inarray(3) = inarray(3)
+
+        If (inarray(4) < 0) Then inarray(4) = 0
+        If (inarray(4) > 1) Then inarray(4) = 1
+        inarray(4) = inarray(4)
+
+        If (inarray(5) < 0) Then inarray(5) = 0
+        If (inarray(5) > 1) Then inarray(5) = 1
+        inarray(5) = inarray(5)
+
+        If (inarray(6) < 0) Then inarray(6) = 0
+        If (inarray(6) > 1) Then inarray(6) = 1
+        inarray(6) = inarray(6)
+
+        netsum = 0.552925
+        netsum = netsum + inarray(1) * 2.624773
+        netsum = netsum + inarray(2) * -0.2116123
+        netsum = netsum + inarray(3) * 0.3384093
+        netsum = netsum + inarray(4) * 0.654878
+        netsum = netsum + inarray(5) * -2.384259
+        netsum = netsum + inarray(6) * 1.012083
+        feature2(1) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = 0.2864033
+        netsum = netsum + inarray(1) * -0.232227
+        netsum = netsum + inarray(2) * -0.2004608
+        netsum = netsum + inarray(3) * 0.3380726
+        netsum = netsum + inarray(4) * 0.3092833
+        netsum = netsum + inarray(5) * -1.9234
+        netsum = netsum + inarray(6) * -1.255504
+        feature2(2) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -0.3202231
+        netsum = netsum + inarray(1) * 1.077787
+        netsum = netsum + inarray(2) * 0.293115
+        netsum = netsum + inarray(3) * 2.355233
+        netsum = netsum + inarray(4) * -1.844521
+        netsum = netsum + inarray(5) * -1.843686
+        netsum = netsum + inarray(6) * -0.5675715
+        feature2(3) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -0.6125705
+        netsum = netsum + inarray(1) * 4.149592
+        netsum = netsum + inarray(2) * -0.03258462
+        netsum = netsum + inarray(3) * -1.024187
+        netsum = netsum + inarray(4) * -1.040119
+        netsum = netsum + inarray(5) * -1.032503
+        netsum = netsum + inarray(6) * 0.4039429
+        feature2(4) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = 0.9853073
+        netsum = netsum + inarray(1) * -1.723817
+        netsum = netsum + inarray(2) * -0.2285501
+        netsum = netsum + inarray(3) * -1.625859
+        netsum = netsum + inarray(4) * 1.237068
+        netsum = netsum + inarray(5) * -3.020462
+        netsum = netsum + inarray(6) * 0.9360621
+        feature2(5) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = 0.4483171
+        netsum = netsum + inarray(1) * 0.135884
+        netsum = netsum + inarray(2) * -0.2971984
+        netsum = netsum + inarray(3) * -1.750201
+        netsum = netsum + inarray(4) * 1.014766
+        netsum = netsum + inarray(5) * 0.1713855
+        netsum = netsum + inarray(6) * -4.692516
+        feature2(6) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -1.815358
+        netsum = netsum + inarray(1) * -1.660086
+        netsum = netsum + inarray(2) * -0.2946532
+        netsum = netsum + inarray(3) * 3.666048
+        netsum = netsum + inarray(4) * -0.1632731
+        netsum = netsum + inarray(5) * 0.05557675
+        netsum = netsum + inarray(6) * -2.592651
+        feature2(7) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -1.468176
+        netsum = netsum + inarray(1) * -0.8806654
+        netsum = netsum + inarray(2) * -0.07327189
+        netsum = netsum + inarray(3) * -2.381941
+        netsum = netsum + inarray(4) * -0.7902038
+        netsum = netsum + inarray(5) * 3.295805
+        netsum = netsum + inarray(6) * -3.010131
+        feature2(8) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = 0.134371
+        netsum = netsum + inarray(1) * -1.801088
+        netsum = netsum + inarray(2) * 0.01899777
+        netsum = netsum + inarray(3) * 0.515391
+        netsum = netsum + inarray(4) * -2.266897
+        netsum = netsum + inarray(5) * -1.211797
+        netsum = netsum + inarray(6) * 3.070935
+        feature2(9) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -2.688084
+        netsum = netsum + feature2(1) * -0.1949707
+        netsum = netsum + feature2(2) * 0.2036091
+        netsum = netsum + feature2(3) * 3.372597
+        netsum = netsum + feature2(4) * 0.4929688
+        netsum = netsum + feature2(5) * -0.6245344
+        netsum = netsum + feature2(6) * 0.6217883
+        netsum = netsum + feature2(7) * -0.4985358
+        netsum = netsum + feature2(8) * 0.1053541
+        netsum = netsum + feature2(9) * 1.815413
+        outarray(1) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -0.7274325
+        netsum = netsum + feature2(1) * 1.398895
+        netsum = netsum + feature2(2) * -1.034414
+        netsum = netsum + feature2(3) * -2.475591
+        netsum = netsum + feature2(4) * 2.815179
+        netsum = netsum + feature2(5) * -1.587131
+        netsum = netsum + feature2(6) * -0.1551941
+        netsum = netsum + feature2(7) * -0.7814186
+        netsum = netsum + feature2(8) * -1.214203
+        netsum = netsum + feature2(9) * -2.923214
+        outarray(2) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = 1.63765
+        netsum = netsum + feature2(1) * -1.489454
+        netsum = netsum + feature2(2) * -0.7080432
+        netsum = netsum + feature2(3) * -1.551334
+        netsum = netsum + feature2(4) * -3.096043
+        netsum = netsum + feature2(5) * 1.53787
+        netsum = netsum + feature2(6) * -3.665871
+        netsum = netsum + feature2(7) * -1.510865
+        netsum = netsum + feature2(8) * -0.552496
+        netsum = netsum + feature2(9) * 2.365625
+        outarray(3) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -3.172832
+        netsum = netsum + feature2(1) * 0.8154854
+        netsum = netsum + feature2(2) * 2.057259
+        netsum = netsum + feature2(3) * 0.6460317
+        netsum = netsum + feature2(4) * -0.1476683
+        netsum = netsum + feature2(5) * 2.572043
+        netsum = netsum + feature2(6) * 1.870373
+        netsum = netsum + feature2(7) * -0.4693826
+        netsum = netsum + feature2(8) * -2.573551
+        netsum = netsum + feature2(9) * -1.562899
+        outarray(4) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -0.4524418
+        netsum = netsum + feature2(1) * -0.1259253
+        netsum = netsum + feature2(2) * 0.3251145
+        netsum = netsum + feature2(3) * 1.024084
+        netsum = netsum + feature2(4) * -1.331062
+        netsum = netsum + feature2(5) * -1.316061
+        netsum = netsum + feature2(6) * -0.9064624
+        netsum = netsum + feature2(7) * 3.965195
+        netsum = netsum + feature2(8) * -1.350906
+        netsum = netsum + feature2(9) * -1.691364
+        outarray(5) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -0.2219889
+        netsum = netsum + feature2(1) * -1.54205
+        netsum = netsum + feature2(2) * -0.1211412
+        netsum = netsum + feature2(3) * 1.083872
+        netsum = netsum + feature2(4) * -0.3466245
+        netsum = netsum + feature2(5) * -2.439729
+        netsum = netsum + feature2(6) * 1.207266
+        netsum = netsum + feature2(7) * 2.094129
+        netsum = netsum + feature2(8) * 3.144963
+        netsum = netsum + feature2(9) * -0.9136803
+        outarray(6) = 1 / (1 + Math.Exp(-netsum))
+
+        netsum = -1.538077
+        netsum = netsum + feature2(1) * -1.419736
+        netsum = netsum + feature2(2) * -0.5824777
+        netsum = netsum + feature2(3) * 0.2159221
+        netsum = netsum + feature2(4) * 0.6310436
+        netsum = netsum + feature2(5) * -1.677176
+        netsum = netsum + feature2(6) * 2.518753
+        netsum = netsum + feature2(7) * -2.358588
+        netsum = netsum + feature2(8) * 3.842276
+        netsum = netsum + feature2(9) * 0.7231038
+        outarray(7) = 1 / (1 + Math.Exp(-netsum))
+
+
+        outarray(1) = (outarray(1) - 0.1) / 0.8
+        If (outarray(1) < 0) Then outarray(1) = 0
+        If (outarray(1) > 1) Then outarray(1) = 1
+
+        outarray(2) = (outarray(2) - 0.1) / 0.8
+        If (outarray(2) < 0) Then outarray(2) = 0
+        If (outarray(2) > 1) Then outarray(2) = 1
+
+        outarray(3) = (outarray(3) - 0.1) / 0.8
+        If (outarray(3) < 0) Then outarray(3) = 0
+        If (outarray(3) > 1) Then outarray(3) = 1
+
+        outarray(4) = (outarray(4) - 0.1) / 0.8
+        If (outarray(4) < 0) Then outarray(4) = 0
+        If (outarray(4) > 1) Then outarray(4) = 1
+
+        outarray(5) = (outarray(5) - 0.1) / 0.8
+        If (outarray(5) < 0) Then outarray(5) = 0
+        If (outarray(5) > 1) Then outarray(5) = 1
+
+        outarray(6) = (outarray(6) - 0.1) / 0.8
+        If (outarray(6) < 0) Then outarray(6) = 0
+        If (outarray(6) > 1) Then outarray(6) = 1
+
+        outarray(7) = (outarray(7) - 0.1) / 0.8
+        If (outarray(7) < 0) Then outarray(7) = 0
+        If (outarray(7) > 1) Then outarray(7) = 1
+
+        Me.TextBox7.Text = outarray(1)
+        Me.TextBox8.Text = outarray(2)
+        Me.TextBox9.Text = outarray(3)
+        Me.TextBox10.Text = outarray(4)
+        Me.TextBox11.Text = outarray(5)
+        Me.TextBox12.Text = outarray(6)
+        Me.TextBox13.Text = outarray(7)
+
+    End Sub
+End Class
